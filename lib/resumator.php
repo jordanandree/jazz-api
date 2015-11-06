357670b2d9be508e4216bb18c811fb5e9b938d34
Rob Widdick <rob@36creative.com>
2015-11-06 15:49:27 -0500

    Implemented basic caching mechanism for GET requests.
    Other improvements:
    * Fix issue with curl handle attempting to close after a return statement in the apiRequest method, resulting in a the handle never getting closed
    * Clean up some pieces of code for readability
    
    Todo: Add tests for caching

--- lib/resumator.php
+++ lib/resumator.php
@@ -1,6 +1,4 @@
 <?php
-
-
 if (!function_exists('curl_init')) {
   throw new Exception('Resumator needs the CURL PHP extension.');
 }
@@ -12,7 +10,6 @@ require_once "inflector.php";
 use Doctrine\Common\Inflector\Inflector as DoctrineInflector;
 
 class Resumator {
-
   /**
    * Class Version
    */
@@ -81,7 +78,6 @@ class Resumator {
     )
   );
 
-
   /**
    * Default options for curl.
    *
@@ -99,6 +95,16 @@ class Resumator {
    * @var string
    */
   protected $api_key;
+
+  /**
+   * Cache configuration
+   *
+   * @var array CACHE_PATH: the absolute path to store cache files, CACHE_EXPIRE: cache validity (in seconds)
+   */
+  protected static $cache = array(
+    "CACHE_PATH"   => __DIR__ . DIRECTORY_SEPARATOR . "cache" . DIRECTORY_SEPARATOR,
+    "CACHE_EXPIRE" => 86400 // 86400 = 24hrs
+  );
 
   /**
    * Initialize a Resumator instance.
@@ -123,7 +129,6 @@ class Resumator {
    * Intercept methods and map to self::$endpoints
    *
    */
-
   public function __call($method_name, $arguments) {
     $inflector = new DoctrineInflector();
     preg_match("/(get|post)/", $method_name, $request_matches);
@@ -146,7 +151,6 @@ class Resumator {
       return $this->apiRequest($endpoint, $arguments[0], "POST");
     # GET requests
     } else {
-
       # method is different than endpoint (singular) and args exists
       # probably asking for a single resource
       if($method !== $endpoint && empty($arguments)) {
@@ -181,10 +185,22 @@ class Resumator {
    * @return object the request response data
    */
   private function apiRequest($endpoint, $params = array(), $http_method = "GET") {
+    $url = $this->buildURL(self::API_URL . self::API_VERSION, $endpoint);
+
+    /**
+     * Only allow caching for GET requests
+     */
+    if($http_method == "GET") {
+      // dynamic cache filename from sha1 hash
+      $cacheFile = sha1($url . json_encode($params));
+      $cache = $this->readCache($cacheFile);
+      if ($cache) {
+        return json_decode($cache);
+      }
+    }
+
     $ch = curl_init();
     $opts = self::$CURL_OPTS;
-
-    $url = $this->buildURL(self::API_URL . self::API_VERSION, $endpoint);
 
     if( isset($params) ) {
       if( $http_method == "POST" ) {
@@ -202,14 +218,14 @@ class Resumator {
     curl_setopt_array($ch, $opts);
     $result = curl_exec($ch);
     $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
 
-    if( $status !== 200 ) {
-      return @json_decode($result);
-    } else {
-      return @json_decode($result);
+    /** Write cache only for GET request */
+    if($http_method == "GET") {
+      $this->writeCache($cacheFile, $result);
     }
 
-    curl_close($ch);
+    return @json_decode($result);
   }
 
   /**
@@ -219,7 +235,6 @@ class Resumator {
    * @param mixed $endpoint a string, normal, or associative array of endpoints
    * @return string the formatted url
    */
-
   private function buildURL($base, $endpoint) {
     $return_url = $base;
     if( gettype($endpoint) == "string") {
@@ -237,4 +252,42 @@ class Resumator {
     return $return_url;
   }
 
+  /**
+   * Read the cache file, if it exists and within the expiration time
+   *
+   * @param bool|false $file the filename, without extension
+   * @return bool|string the cached data, or false on failure
+   */
+  private function readCache($file = false) {
+    $cache = self::$cache['CACHE_PATH'] . $file . ".cache";
+    if(file_exists($cache)) {
+      if(filemtime($cache) > time() - self::$cache['CACHE_EXPIRE']) {
+        // Returned cached data
+        $data = @file_get_contents($cache);
+        return $data;
+      } else {
+        // Attempt to delete the cache file
+        @unlink($cache);
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Write to the specified cache file
+   *
+   * @param bool|false $file the filename, without extension
+   * @param string $data the data to write
+   * @return bool the result of the write
+   */
+  private function writeCache($file = false, $data = "") {
+    $cache = self::$cache['CACHE_PATH'] . $file . ".cache";
+    $fp = fopen($cache, "w");
+    if($fp) {
+      $result = fwrite($fp, $data);
+      fclose($fp);
+      return $result;
+    }
+    return false;
+  }
 }
